3492e0aef34e828009d084a687a80c74ab84125b
Gustavo Fanha <fanhagustavo@gmail.com>
2024-12-08 21:29:31 -0300

    Created OrderStatus

--- src/main/java/com/fanhagustavo/learning/config/TestConfig.java
+++ src/main/java/com/fanhagustavo/learning/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.fanhagustavo.learning.config;
 
 import com.fanhagustavo.learning.entities.Order;
 import com.fanhagustavo.learning.entities.User;
+import com.fanhagustavo.learning.entities.enums.OrderStatus;
 import com.fanhagustavo.learning.repositories.OrderRepository;
 import com.fanhagustavo.learning.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "José Almeida", "josealmeida@gmail.com", "988888888", "123456");
         User user2 = new User(null, "Evandro Sales", "evandrosales@gmail.com", "977777777", "123456");
 
-        Order order1 = new Order(null, Instant.parse("2022-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2022-07-21T20:00:00Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2022-06-25T17:32:10Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2022-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2022-07-21T20:00:00Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2022-06-25T17:32:10Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
